6909ada89fef80f6d8a48862e35dd08101a0e1c3
Fred CHARLOT <fred_ch13@yahoo.fr>
2020-12-12 13:39:26 +0100

    Create Sclient.java

---
+++ Sclient.java
@@ -0,0 +1,10 @@
+
+public class Sclient {
+
+	public static void main(String[] args) {
+		//PartieClient partieClient = new PartieClient();
+		//partieClient.deroulement();
+		MenuDebut menu= new MenuDebut();
+	}
+
+}
