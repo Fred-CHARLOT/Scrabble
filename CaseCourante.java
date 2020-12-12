3b9c9b4e7ee0a8e04ee9597b70ae2733bbd0748d
Fred CHARLOT <fred_ch13@yahoo.fr>
2020-12-12 13:39:02 +0100

    Create CaseCourante.java

---
+++ CaseCourante.java
@@ -0,0 +1,14 @@
+
+
+
+public class CaseCourante {
+public int ligne, colonne;
+public String bonus,lettre;
+	
+	CaseCourante(int ligne, int colonne, String bonus, String lettre){
+		this.ligne=ligne;
+		this.colonne=colonne;
+		this.bonus=bonus;
+		this.lettre=lettre;
+	}
+}
