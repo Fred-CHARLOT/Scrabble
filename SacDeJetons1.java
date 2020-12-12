72032fb68fe293b1e1a372848f023fc8d8f94e9b
Fred CHARLOT <fred_ch13@yahoo.fr>
2020-12-12 13:39:23 +0100

    Create SacDeJetons1.java

---
+++ SacDeJetons1.java
@@ -0,0 +1,40 @@
+
+
+import java.util.ArrayList;
+
+public class SacDeJetons {
+	ArrayList <String> sac = new ArrayList<String>() ;
+	
+		SacDeJetons(){
+			for (int i=0;i<=14;i++) {sac.add("E");}
+			for (int i=15;i<=23;i++){sac.add("A");}
+			for (int i=24;i<=31;i++) {sac.add("I");}
+			for (int i=32;i<=37;i++) {sac.add("N");}
+			for (int i=38;i<=43;i++) {sac.add("O");}
+			for (int i=44;i<=49;i++) {sac.add("R");}
+			for (int i=50;i<=55;i++) {sac.add("S");}
+			for (int i=56;i<=61;i++) {sac.add("T");}
+			for (int i=62;i<=66;i++) {sac.add("U");}
+			for (int i=68;i<=72;i++) {sac.add("L");}
+			for (int i=73;i<=75;i++) {sac.add("D");}
+			for (int i=76;i<=78;i++) {sac.add("M");}
+			for (int i=79;i<=80;i++) {sac.add("G");}
+			for (int i=81;i<=82;i++) {sac.add("B");}
+			for (int i=83;i<=84;i++) {sac.add("C");}
+			for (int i=85;i<=86;i++) {sac.add("P");}
+			for (int i=87;i<=88;i++) {sac.add("F");}
+			for (int i=89;i<=90;i++) {sac.add("H");}
+			for (int i=91;i<=92;i++) {sac.add("V");}
+			for (int i=93;i<=94;i++) {sac.add(" ");}
+			sac.add("J");
+			sac.add("Q");
+			sac.add("K");
+			sac.add("W");
+			sac.add("X");
+			sac.add("Y");
+			sac.add("Z");
+		}
+	
+	
+
+}
