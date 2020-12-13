822b1d8de5edf427f5e5cba61f208c6bd8a742bb
jb <jb@COBRA>
2020-12-13 13:59:47 +0100

    Test d'affichage, 13h59

--- Sclient.java
+++ Sclient.java
@@ -2,9 +2,10 @@
 public class Sclient {
 
 	public static void main(String[] args) {
-		//PartieClient partieClient = new PartieClient();
-		//partieClient.deroulement();
-		MenuDebut menu= new MenuDebut();
+		PartieClient partieClient = new PartieClient();
+		partieClient.deroulement();
+		//MenuDebut menu= new MenuDebut();
+		System.out.println("nouyveau test 13h52");
 	}
 
 }
