7be02961ec03ec8cd8dd3219a8d629ff3d2e4088
Fred CHARLOT <fred_ch13@yahoo.fr>
2020-12-16 09:54:22 +0100

    Create CoupCourant.java

---
+++ CoupCourant.java
@@ -0,0 +1,11 @@
+
+public class CoupCourant {
+public int ligne, colonne;
+public String valeur;
+	
+	CoupCourant(int ligne, int colonne, String valeur){
+		this.ligne=ligne;
+		this.colonne=colonne;
+		this.valeur=valeur;
+	}
+}
