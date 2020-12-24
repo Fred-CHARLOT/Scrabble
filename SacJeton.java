fa516dd702f55b0417931e52df51e94b996ea64f
Fred CHARLOT <fred_ch13@yahoo.fr>
2020-12-24 10:13:05 +0100

    Update SacJeton.java

--- SacJeton.java
+++ SacJeton.java
@@ -104,7 +104,14 @@ public class SacJeton {
 		return a;
 	}	
 	
-	
+	public String[] recupjetons(int n) { /* n est le nombre de jeton à remplacer */
+		
+		n=((this.sac_jeton).size()<=n)?n:(this.sac_jeton).size();
+		String[] b = new String[n];
+		b  = this.tirage(n);
+		return b;
+	}
+		
 	
 		
 		
