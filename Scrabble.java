7decaaf9b08db23b5d73f6d04bb1a1e76d6b5bdb
Fred CHARLOT <fred_ch13@yahoo.fr>
2020-12-30 19:00:39 +0100

    Update Scrabble.java

--- Scrabble.java
+++ Scrabble.java
@@ -1,11 +1,9 @@
 
-
 public class Scrabble {
-	
-	
 	public static void main(String[] args) {
-	final int nombreDeJoueur =2; //plus tard à mettre en argument ou poser la question
-	Partie partieServeur = new Partie(nombreDeJoueur)	;
-	partieServeur.deroulement();
+		final int nombreDeJoueur =2; //plus tard à mettre en argument ou poser la question
+		Partie partieServeur = new Partie(nombreDeJoueur)	;
+		partieServeur.deroulement();
+		}
 	}
-}
+
