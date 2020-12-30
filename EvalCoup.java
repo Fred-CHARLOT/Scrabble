60a18a45bfc4218025802f3389287801d59f4611
Fred CHARLOT <fred_ch13@yahoo.fr>
2020-12-30 18:59:10 +0100

    Update EvalCoup.java
    mise a jour

--- EvalCoup.java
+++ EvalCoup.java
@@ -4,23 +4,11 @@ import java.util.List;
 
 public class EvalCoup {
 	
-	ArrayList <CaseCourante[]> mots;
+					
+//suppression du constructeur	
 	
 	
-		
-	/*public static boolean MotEnLigne (String[] mot) {
-		if ((mot[0]).charAt(0) ==(mot[1]).charAt(0)) return true;
-		else return false;
-	}*/
-				
-	EvalCoup(ArrayList <CaseCourante[]> EnsMots) {  // c'est un test. Ici je pense qu'il faut supprimer le constructeur.
-		
-		this.mots=EnsMots;
-		
-	}
-	
-	
-	public static int SommeLettres (CaseCourante[] coup) {   
+	private int sommeLettres (CaseCourante[] coup) {   
 		
 			int somme = 0;
 			/*for (int k=0; k<coup.size();k++)*/
@@ -30,7 +18,7 @@ public class EvalCoup {
 				int b = k.valeur;
 				/*System.out.println(b);*/
 				String c = k.bonus;
-				/*System.out.println(c)*/
+				/*System.out.println(c)*/				
 				if (c.equals("LD")) {somme = somme + 2*b;}
 				if (c.equals("LT")) {somme = somme + 3*b;}
 				if (!c.equals("LD") && !c.equals("LT")) {somme = somme + b;}
@@ -41,17 +29,17 @@ public class EvalCoup {
 	
 		
 	
-	public static int ScoreMot(CaseCourante[] coup) {
+	private int scoreMot(CaseCourante[] coup) {
 			
-			int score = SommeLettres(coup);
+			int score = sommeLettres(coup);
 			int Dcoeff = 1;
 			int Tcoeff = 1;
 			
 			for (var k:coup) {
 				
 				String c = k.bonus;
-				if (c.equals("MD")) {Dcoeff=2*Dcoeff;}
-				if (c.equals("MT")) {Tcoeff=3*Tcoeff;}
+				if (c.equals("MD")||(c.equals("*"))) {Dcoeff=2*Dcoeff;}
+				if (c.equals("MT")) {Tcoeff=3*Tcoeff;}				
 			}
 			
 				score = score * Dcoeff * Tcoeff;
@@ -60,61 +48,36 @@ public class EvalCoup {
 			return score;
 		
 		}
-			
 	
-	/*public ArrayList <CaseCourante[]> EnsMots(ArrayList <String[]> coup) {
-			ArrayList <CaseCourante[]> mot = new ArrayList <CaseCourante[]>();
+//ajout de la liste en parametre				
+	public int scoreCoup(ArrayList <CaseCourante[]> ensMots) {
 		
-			for (int k=0;k<coup.size(); k++) {
-			System.out.println(k);
-			
-				CaseCourante[] extrait = new CaseCourante[coup.get(k).length];
-			
-			for (int j=0; j<coup.get(k).length;j++) {
-				
-				int ligne = Character.getNumericValue(coup.get(k)[j].charAt(0));
-				System.out.print(ligne);
-				int colonne = Character.getNumericValue(coup.get(k)[j].charAt(1));
-				System.out.print(colonne);
-				String bonus = coup.get(k)[j].substring(2,4);
-				System.out.print(bonus);
-				String lettre = coup.get(k)[j].substring(4,5);
-				System.out.print(lettre);
-				int valeur;
-				if (coup.get(k)[j].length()==6) {valeur = Character.getNumericValue(coup.get(k)[j].charAt(5));}
-					else {valeur=Integer.parseInt(coup.get(k)[j].substring(5,7));}
-				System.out.println(valeur);
-				CaseCourante mot1 = new CaseCourante(ligne,colonne,bonus,lettre,valeur);
-				extrait[j]=mot1;
-			  }
-			mot.add(extrait);
-			}
-		
-		return mot;
-						
-	}*/
-	
-				
-	public int ScoreCoup() {
-		
-		int Score = 0;
-			for (int k=0;k<this.mots.size();k++) {
-				CaseCourante[] mot = (this.mots).get(k);
-				Score = Score + ScoreMot(mot);}
-		return Score;
+		int score = 0;
+			for (int k=0;k<ensMots.size();k++) {
+				CaseCourante[] mot = ensMots.get(k);
+				score = score + scoreMot(mot);}
+		return score;
 		
 	}
 			   
 		
 			
-	public static int MiseAJourScore (int score, int scorecoup) {
-		score = score + scorecoup;
-	return score;
-		
+	public  int miseAJourScore (int score, ArrayList <CaseCourante[]> ensMots) {
+		score = score + scoreCoup(ensMots);
+		System.out.println(ensMots.get(0)[1].valeur);
+		return score;
 	}
 	
 	
+	public  int scoreFinal(int score, CaseCourante[] chevalet) {
+		
+		score=score - sommeLettres(chevalet);
+		return score;
+	}
 	
+public  int scoreFinal(int score, String [] reglette) {
+		return score;
+	}
 	
 		
 		}
@@ -138,22 +101,4 @@ public class EvalCoup {
 			
 			
 			
-			
-			
-		
-		
-		
-		
-		
-		
 	
-	
-	
-	
-	
-	
-	
-	
-	
-
-
