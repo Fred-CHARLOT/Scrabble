abc92a07d27876184115b5a1d2824217b9faf509
Fred CHARLOT <fred_ch13@yahoo.fr>
2021-01-05 16:30:45 +0100

    Update PartieClient.java
    modif jb

--- PartieClient.java
+++ PartieClient.java
@@ -9,14 +9,17 @@ import javax.management.StringValueExp;
 
 public class PartieClient {
 	boolean partieEnCours;		
-	static boolean joueurAjoue;
-	static boolean joueurAPasse;
-	static boolean joueurAChange;
+	
+	//static boolean joueurAjoue;
+	//static boolean joueurAPasse;
+	//static boolean joueurAChange;
+	
 	final boolean OnAttendQueLeJoueurJoue=true;
+	private boolean joueurAfini=false;
 	static int scoreJoueur1=0;
 	static int scoreServeur=0;
 	private int tirage;
-	int JetonsRestant;
+	int JetonsRestant=88;
 	public String nomServeur, nomJoueur1="JB";
 	Chevalet chevalet;
 	GestionGrille grille; 
@@ -46,19 +49,30 @@ public class PartieClient {
 	public void deroulement() {
 		partieEnCours =true;			 
 		tirage=(int)recevoirObjet();
+		System.out.println(tirage);
 		if (tirage==1)premierTour();
 		
 		while (partieEnCours == true) {	    				
-			ilJoue() ;		
-			
+			ilJoue() ;				
 			jeJoue();			 
-			if ((partieEnCours==false)||joueurAfini()) {
+			if ((partieEnCours==false)||joueurAfini) {
 				chevalet.videCoup();
 				envoyerObjet(chevalet.reglette);
 				finDePartie();				
 			}
 		}	
 	}
+	
+	private void premierTour() {	//a verifier	
+		jeJoue();			 
+		//envoyer();
+		//chevalet.majReglette((String [])recevoirObjet());	
+	}
+	
+	
+	
+	
+	
 	
 	//Creer une array liste de tableau de  cases courantes pour tester Evalcoup. Attention la valeur est à zéro.
 	private ArrayList <CaseCourante[]> test (ArrayList<CaseCourante> liste){
@@ -83,8 +97,7 @@ public class PartieClient {
 	private void envoyer () {
 		envoyerObjet(Plateau.plateau); 
 		envoyerObjet(scoreJoueur1);	
-		envoyerObjet(joueurAfini());		
-				
+		envoyerObjet(joueurAfini);						
 	}
 	
 	
@@ -97,18 +110,12 @@ public class PartieClient {
 		}
 	}
 	
-	private void premierTour() {	//a verifier	
-		jeJoue();			 
-		envoyer();
-		chevalet.coup.clear();
-		chevalet.majReglette((String [])recevoirObjet());	
-	}
+	
 	
 	
 	private void ilJoue() {
-		joueurAjoue=true;		
-		try {			
-				
+		chevalet.joueurAjoue=true;		
+		try {					
 			Plateau.plateau=(String [] [])client.in.readObject();		  	
 			if (chevalet.coup.size()!=0)chevalet.videCoup();                  
 			grille.remplirCases(Plateau.plateau);      
@@ -117,7 +124,7 @@ public class PartieClient {
 			JetonsRestant=(int) client.in.readObject();        
 			grille.jetonsRestant.setText("il reste " + JetonsRestant + " jetons");
 			partieEnCours=(boolean) client.in.readObject();
-            joueurAjoue=false;
+            chevalet.joueurAjoue=false;
             if (partieEnCours==false) {
             	System.out.println("partie fini");
             	chevalet.videCoup();
@@ -132,46 +139,46 @@ public class PartieClient {
 	
 	
 	private void jeJoue() {
-		joueurAjoue=false;
-		joueurAPasse=false;
-		joueurAChange=false;
+		chevalet.joueurAjoue=false;
+		chevalet.joueurAPasse=false;
+		chevalet.joueurAchange=false;
 		while (OnAttendQueLeJoueurJoue) {	     		  
 			chevalet.valider.setBackground(Color.green);		   
-			if (joueurAjoue==true) { 	    	
-				envoyerObjet(joueurAPasse);
-					if (joueurAPasse) {
+			if (chevalet.joueurAjoue==true) { 	    	
+				
+				envoyerObjet(chevalet.joueurAPasse);
+					if (chevalet.joueurAPasse) {
 					grille.remplirCases(Plateau.plateau);
 					envoyerObjet(Plateau.plateau);  //sans ça ça marche pas: ma foi.....
 					partieEnCours=(boolean) recevoirObjet();					
 					break;	
 				}	    	
 				
-				envoyerObjet(joueurAChange);		
-				if (joueurAChange){
+				envoyerObjet(chevalet.joueurAchange);		
+				if (chevalet.joueurAchange){
 					//chevalet.videCoup();
 					//grille.remplirCases(Plateau.plateau);
 					envoyerObjet(Plateau.plateau);
-					changeJetons();
-					
+					changeJetons();					
 					break;}	
 	     	
-	     	
-				// Dans les autres cas :
+	     	// Dans les autres cas :
 				//extraction de la Array liste  de tableau de coups joués.
 				scoreJoueur1=scoreJoueur1+eval.scoreCoup(test(chevalet.coup));//il faudra remplacer chavalet.coup par la Aray liste.
 				miseAJourTableau();
 				grille.remplirCases(Plateau.plateau);//(pour enlever les couleurs)
 				grille.score1.setText(nomJoueur1 + " : "+ String.valueOf(scoreJoueur1));
 				if (JetonsRestant-chevalet.jetonsAChanger>=0)grille.jetonsRestant.setText("il reste " + (JetonsRestant-chevalet.jetonsAChanger) + " jetons");
-				envoyer();
 				chevalet.coup.clear();
-				if (joueurAfini())   break;
+				joueurAfini=joueurAfini();
+				envoyer();
+				
+				if (joueurAfini)   break;
 					//sinon on récupère de nouveaux jetons
 					envoyerObjet(chevalet.getJetonsAChanger());	     	
 					reglette=(String [])recevoirObjet();
-					//System.out.println(reglette.length);
-					chevalet.majReglette(reglette);
-					for (int i =0; i<chevalet.reglette.length;i++)System.out.println(chevalet.reglette[i]);
+					//for (int i =0; i<chevalet.reglette.length;i++)System.out.println(chevalet.reglette[i]);
+					chevalet.majReglette(reglette);					
 					break;
 	     	}
 	     }		
@@ -181,31 +188,25 @@ public class PartieClient {
 		envoyerObjet((String[])chevalet.echange.lettresAChanger);		
 		reglette=(String[] )recevoirObjet();		
 		chevalet.majReglette(reglette);		
-		joueurAChange=false;
+		chevalet.joueurAchange=false;
 	}
 	
 	
 	
 	private boolean joueurAfini() {
-		chevalet.videCoup();
 		return ((JetonsRestant==0)&& chevalet.isVide(chevalet.reglette));
 	}
 	
 	
 	
-	private void finDePartie() {
-	
+	private void finDePartie() {	
 		try { 	     			
 			 Thread.sleep (5000);
 		    } catch (Exception e) {
 		        e.printStackTrace();
-		    }
-		
+		    }		
 		fermeture();
 	}
-	
-	
-	
 	
 	
 	void miseAJourTableau() {
@@ -221,12 +222,11 @@ public class PartieClient {
 		   	System.exit(0);
 			} catch (Exception e) {
 			     e.printStackTrace();
-			    }			
-		  System.out.println("fin3");
+			    }		
 	}
 	
 	
-	}
+}
 		
 			
 		
